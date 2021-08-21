22c341bfcacad1a2cc6b2c2f283fb8174450942a
duonghungit24 <hungduonng721@gmail.com>
2021-08-21 22:14:15 +0700

    update controller skipNext and skipPrevious

--- component/musicPlayer.js
+++ component/musicPlayer.js
@@ -1,4 +1,4 @@
-import React ,{useEffect , useRef , useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   View,
   Text,
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
   Image,
   FlatList,
-  Animated
+  Animated,
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Slider from '@react-native-community/slider';
@@ -17,20 +17,35 @@ import listSongs from '../model/data';
 const {height, width} = Dimensions.get('window');
 
 const MusicPlay = () => {
-  const scrollX = useRef(new Animated.Value(0)).current;
+  const scrollX = useRef(new Animated.Value(0)).current;  // useRef để lưu giá trị hiện tại {current}
   const [songIndex, setSongIndex] = useState(0);
-  useEffect(()=> {
-    scrollX.addListener(({value}) => {           //thêm sự kiện cho scroll để lấy giá trị 
-       const index = Math.round(value / width); // lấy index;
-        setSongIndex(index);
-    })
+  useEffect(() => {             //useEffect giống componenetDidMount,WillDidMount để sử lý sau khi mount
+    scrollX.addListener(({value}) => {
+      //thêm sự kiện cho scroll để lấy giá trị
+      const index = Math.round(value / width); // lấy index;
+      setSongIndex(index);
+    });
     return () => {
-      scrollX.removeAllListeners();
-    }
-  },[]);
-  const renderSong = ({index,item}) => {
+      scrollX.removeAllListeners();  // hủy sử kiện scroll
+    };
+  }, []);
+  const songSlide = useRef(null);
+
+  const skipNext = () => {
+    songSlide.current.scrollToOffset({
+      offset:(songIndex+1)*width
+    })
+  }
+  const skipPrevious = () => {
+    songSlide.current.scrollToOffset({
+      offset:(songIndex-1)*width
+    })
+  }
+
+  const renderSong = ({index, item}) => {
     return (
-      <Animated.View style={{width:width, justifyContent:'center', alignItems:'center'}}>
+      <Animated.View
+        style={{width: width, justifyContent: 'center', alignItems: 'center'}}>
         <View style={styles.imageWrapper}>
           <Image style={styles.image} source={item.image} />
         </View>
@@ -41,21 +56,22 @@ const MusicPlay = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.main}>
-        <Animated.FlatList
-          data={listSongs}
-          renderItem={renderSong}
-          keyExtractor={item => item.id}
-          horizontal
-          pagingEnabled    //dùng để phân từng trang tránh bị trượt
-          showsHorizontalScrollIndicator = {false}
-          scrollEventThrottle = {16}
-          onScroll = {Animated.event(
-            [{nativeEvent: {contentOffset: {x: scrollX}}}],
-            {useNativeDriver:true}
-          )
-
-          }
-        />
+        <View style={{width:width}}>
+          <Animated.FlatList
+            ref={songSlide}
+            data={listSongs}
+            renderItem={renderSong}
+            keyExtractor={item => item.id}
+            horizontal
+            pagingEnabled //dùng để phân từng trang tránh bị trượt
+            showsHorizontalScrollIndicator={false}
+            scrollEventThrottle={16}
+            onScroll={Animated.event(
+              [{nativeEvent: {contentOffset: {x: scrollX}}}],
+              {useNativeDriver: true},
+            )}
+          />
+        </View>
         <View>
           <Text style={styles.title}>{listSongs[songIndex].title}</Text>
           <Text style={styles.author}>{listSongs[songIndex].author}</Text>
@@ -77,13 +93,13 @@ const MusicPlay = () => {
           <Text style={styles.timeEnd}>4:00</Text>
         </View>
         <View style={styles.controller}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={skipPrevious}>
             <Ionicons name="play-skip-back-outline" size={35} color="#fff" />
           </TouchableOpacity>
           <TouchableOpacity>
             <Ionicons name="pause-outline" size={35} color="#fff" />
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={skipNext}>
             <Ionicons name="play-skip-forward-outline" size={35} color="#fff" />
           </TouchableOpacity>
         </View>
@@ -140,7 +156,7 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
-    borderRadius: 300/2,
+    borderRadius: 300 / 2,
   },
   title: {
     fontSize: 18,
@@ -153,7 +169,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: '300',
     color: '#fff',
-    paddingVertical:5,
+    paddingVertical: 5,
   },
   slider: {
     width: 350,
@@ -177,7 +193,7 @@ const styles = StyleSheet.create({
     width: '50%',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginTop: 10,
+    marginTop: 15,
   },
   bottom: {
     width: width,
