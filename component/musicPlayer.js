88bf3f1807e7d1bf383a97ead53bb85bf03ca24d
duonghungit24 <hungduonng721@gmail.com>
2021-08-24 22:45:47 +0700

    set up player and service

--- component/musicPlayer.js
+++ component/musicPlayer.js
@@ -13,14 +13,40 @@ import {
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Slider from '@react-native-community/slider';
 import listSongs from '../model/data';
-import TrackPlayer from 'react-native-track-player';
-
+import TrackPlayer , { 
+  RepeatMode, 
+  State,
+  usePlaybackState,
+  useProgress,
+  useTrackPlayerEvents
+} from 'react-native-track-player';
 const {height, width} = Dimensions.get('window');
 
+
+const setUpPlayers = async () => {   // thiết lập players 
+    await TrackPlayer.setupPlayer()
+    await TrackPlayer.add(listSongs); // add songs in list queue
+}
+
+const musicPlay = async (playbackState) => {           // lấy ra bài nhạc đang phát
+  const trackIndex = await TrackPlayer.getCurrentTrack();
+  if(trackIndex != null ){
+    if(playbackState == State.Paused){            /// nếu nó đang dừng thì nhấn sẽ phát
+      await TrackPlayer.play();
+    }
+    else { 
+      await TrackPlayer.pause(); //đang phát thì nhấn sẽ dừng
+    }
+  }
+}
 const MusicPlay = () => {
+  const playbackState =  usePlaybackState(); //lấy ra trạng thái đang phát hay dừng của bài hát
+
   const scrollX = useRef(new Animated.Value(0)).current;  // useRef để lưu giá trị hiện tại {current}
+
   const [songIndex, setSongIndex] = useState(0);
   useEffect(() => {             //useEffect giống componenetDidMount,WillDidMount để sử lý sau khi mount
+    setUpPlayers(); //goi ham` 
     scrollX.addListener(({value}) => {
       //thêm sự kiện cho scroll để lấy giá trị
       const index = Math.round(value / width); // lấy index;
@@ -42,7 +68,7 @@ const MusicPlay = () => {
       offset:(songIndex-1)*width
     })
   }
-
+  
   const renderSong = ({index, item}) => {
     return (
       <Animated.View
@@ -97,8 +123,8 @@ const MusicPlay = () => {
           <TouchableOpacity onPress={skipPrevious}>
             <Ionicons name="play-skip-back-outline" size={35} color="#fff" style={{marginTop:15}}/>
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Ionicons name="pause-circle-outline" size={60} color="#fff"/>
+          <TouchableOpacity onPress = {() => musicPlay(playbackState)}>
+            <Ionicons name={playbackState == State.Playing ? "pause-circle-outline" : "play-circle-outline" } size={60} color="#fff"/>
           </TouchableOpacity>
           <TouchableOpacity onPress={skipNext}>
             <Ionicons name="play-skip-forward-outline" size={35} color="#fff" style={{marginTop:15}} />
